fa54a45b2f8fa70871482ff0903424cb91c66789
tonesossai77-png <tonesossai77@gmail.com>
2026-02-12 18:07:11 -0700

    Update test.py
    made roof triangle

--- test.py
+++ test.py
@@ -8,19 +8,19 @@ display.set_caption("A Very Big House")
 window.fill("cyan")
 
 # set up objects
-
 background = pygame.Rect(0, 220, 600, 400)
 house = pygame.Rect(90, 180, 300, 400)
 door = pygame.Rect(190, 415, 100, 140)
 window1 = pygame.Rect(110, 315, 60, 60)
 window2 = pygame.Rect(310, 315, 60, 60)
 window1_1 = pygame.Rect(110, 315, 30, 60)
-window1_2 = pygame.Rect(310, 315, 30, 60)
+window1_2 = pygame.Rect(340, 315, 30, 60)
 doorKnob = pygame.Rect(265, 484, 15, 5)
-roof = pygame.Rect (80, 140, 320, 50)
 
-#draw house objects
+# triangle roof (3 points)
+roof_points = [(80, 180), (240, 100), (400, 180)]
 
+# draw house objects
 draw.rect(window, "green", background)
 draw.rect(window, "pink", house)
 draw.rect(window, "black", door)
@@ -29,14 +29,18 @@ draw.rect(window, "blue", window2)
 draw.rect(window, "red", doorKnob)
 draw.rect(window, "grey", window1_1)
 draw.rect(window, "grey", window1_2)
-draw.rect(window, "black", roof)
+
+# draw triangle roof
+draw.polygon(window, "black", roof_points)
+
 display.flip()
 
-#update display
+# update display
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # ✅ handles close button
+        if event.type == pygame.QUIT:
             running = False
 
 pygame.quit()
+
