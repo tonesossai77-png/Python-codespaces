650a7320c408126712d8947e2ad7b5f28441a283
tonesossai77-png <tonesossai77@gmail.com>
2026-02-19 18:07:02 -0700

    Update test.py
    made tree look like a Christmas tree

--- test.py
+++ test.py
@@ -16,13 +16,11 @@ window2 = pygame.Rect(310, 315, 60, 60)
 window1_1 = pygame.Rect(110, 315, 30, 60)
 window1_2 = pygame.Rect(340, 315, 30, 60)
 doorKnob = pygame.Rect(265, 484, 15, 5)
-tree=pygame.Rect(470,420,40,150)
 
 # triangle roof (3 points)
 roof_points = [(80, 180), (240, 100), (400, 180)]
-tree_ponits=[(430, 420), (550, 420), (490, 330)]
 
-# draw house objects
+# draw background + house
 draw.rect(window, "green", background)
 draw.rect(window, "pink", house)
 draw.rect(window, "black", door)
@@ -31,11 +29,23 @@ draw.rect(window, "blue", window2)
 draw.rect(window, "red", doorKnob)
 draw.rect(window, "grey", window1_1)
 draw.rect(window, "grey", window1_2)
-draw.circle(window, "yellow",(500, 80),50 )
-draw.rect(window, "brown", tree)
-# draw triangle roof
+draw.circle(window, "yellow", (500, 80), 50)
+
+# draw roof
 draw.polygon(window, "black", roof_points)
-draw.polygon(window, "dark green", tree_ponits)
+
+# 🎄 Christmas Tree
+
+# Tree trunk
+draw.rect(window, "brown", (470, 470, 40, 100))
+
+# Tree layers
+draw.polygon(window, "dark green", [(440, 470), (540, 470), (490, 400)])
+draw.polygon(window, "dark green", [(450, 430), (530, 430), (490, 360)])
+draw.polygon(window, "dark green", [(460, 390), (520, 390), (490, 320)])
+
+
+
 display.flip()
 
 # update display
